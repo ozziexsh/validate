56710e3361a133fa3436eb1464a77f45ac8d9c1e
Ozzie Neher <oz.neher@gmail.com>
2023-12-24 10:17:41 -0500

    move if to guard

--- lib/validate/rules/size.ex
+++ lib/validate/rules/size.ex
@@ -8,7 +8,7 @@ defmodule Validate.Rules.Size do
     message = "must have a size of exactly #{size}"
 
     case Util.get_size(value) do
-      x when is_number(x) -> if x == size, do: success(value), else: error(message)
+      x when is_number(x) and x == size -> success(value)
       _ -> error(message)
     end
   end
